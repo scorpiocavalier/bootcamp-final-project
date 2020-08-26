ac4cc740cdbbb42971e9d822e9d3f918a5657382
Arturo Chen <art.can.fly@gmail.com>
2020-08-25 21:42:22 -0400

    + graphql add and get product from/to mongodb

---
+++ server/models/product.js
@@ -0,0 +1,18 @@
+import { Schema, model } from 'mongoose'
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  itemCode: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+})
+
+export const Product = model('Product', productSchema)
--- server/server.js
+++ server/server.js
@@ -5,27 +5,48 @@ import morgan from 'morgan'
 import cors from 'cors'
 import { graphqlHTTP } from 'express-graphql'
 import { buildSchema } from 'graphql'
+import { connect, connection } from 'mongoose'
+
+import { Product } from './models/product'
 
 // Import the secrets
 config()
-const { PORT } = process.env
+const { PORT, MONGODB_URI } = process.env
 
 // Setup express app
 const app = express()
 app.use(bodyParser.json())
 app.use(morgan('dev'))
-app.use(cors)
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Setup graphql
 app.use(
   '/graphql',
   graphqlHTTP({
-    graphiql: true,
     schema: buildSchema(`
-      type RootQuery {
+      type Product {
+        _id: ID,
+        name: String!,
+        itemCode: String!,
+        price: Float!
+      }
 
+      input ProductInput {
+        name: String!
+        itemCode: String!
+        price: Float!
+      }
+
+      type RootQuery {
+        products: [Product!]!
       }
 
       type RootMutation {
-
+        createProduct(productInput: ProductInput): Product
       }
 
       schema {
@@ -33,9 +54,51 @@ app.use(
         mutation: RootMutation
       }
     `),
-    rootValue: {}
+    rootValue: {
+      products: () => {
+        // fetch all from db
+        return Product
+          .find()
+          .then(products => {
+            console.log('products', products)
+            return products.map(product => {
+              return { ...product._doc, _id: product.id }
+            })
+          })
+          .catch(err => {
+            console.log('err', err);
+            throw err
+          })
+      },
+      createProduct: (args) => {
+        const product = new Product({
+          name: args.productInput.name,
+          itemCode: args.productInput.itemCode,
+          price: +args.productInput.price,
+        })
+
+        // add to db
+        return product
+          .save()
+          .then(result => {
+            console.log('result', result)
+            return { ...result._doc, _id: product.id }
+          })
+          .catch(err => {
+            console.log('err', err)
+            throw err
+          })
+      }
+    },
+    graphiql: true
   })
 )
+
+// Connect to mongodb
+connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = connection
+db.on('error', console.error.bind(console, 'conection error:'))
+db.once('open', () => console.log('we are connected!'))
 
 // Start the server
 app.listen(PORT, () => console.info(`Listenning on port ${ PORT }`))
