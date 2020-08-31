928953a771cf823538f28bc4c8b5b47f71b01b85
Arturo Chen <art.can.fly@gmail.com>
2020-08-30 23:50:04 -0400

    + Apollo Provider & Client

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppProvider as PolarisProvider } from '@shopify/polaris'
+import { render } from 'react-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
+import { AppProvider as PolarisProvider } from '@shopify/polaris'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 import App from './components/App'
 import { ContextProvider } from './Context'
 
-ReactDOM.render(
-	<Auth0Provider
-		domain={process.env.REACT_APP_AUTH0_DOMAIN}
-		clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-		redirectUri={window.location.origin}>
-		<PolarisProvider>
-			<ContextProvider>
-				<App />
-			</ContextProvider>
-		</PolarisProvider>
-	</Auth0Provider>,
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+})
+
+render(
+	<ApolloProvider client={client}>
+		<Auth0Provider
+			domain={process.env.REACT_APP_AUTH0_DOMAIN}
+			clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+			redirectUri={window.location.origin}>
+			<PolarisProvider>
+				<ContextProvider>
+					<App />
+				</ContextProvider>
+			</PolarisProvider>
+		</Auth0Provider>
+	</ApolloProvider>,
 	document.getElementById('root')
 )
